55a52b0f457af83d17d1baebba199120b3c6f560
Aliezzar <aliezzar42@gmail.com>
2024-12-07 00:54:40 +0700

    first commit

--- javascript/script.js
+++ javascript/script.js
@@ -10,7 +10,7 @@ const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', function () {
-    nav.classList.toggle('slide')
+    nav.classList.toggle('slide')s
 });
 
 // untuk typing
