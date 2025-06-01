5d7dc5887322ac92fe452402ea5f16eb573f325f
Aliezzar <aliezzar42@gmail.com>
2025-06-02 03:10:21 +0700

    chore(database): clean up SQL file and remove unused ERD diagram
    - Remove test data and clean up SQL file structure
    - Delete unused ERD diagram file
    - Update table comments to Indonesian language
    - Simplify users table data

--- .database/tanikita.sql
+++ .database/tanikita.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 25, 2025 at 12:47 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 01 Jun 2025 pada 21.59
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `history`
+-- Struktur dari tabel `history`
 --
 
 CREATE TABLE `history` (
@@ -34,79 +34,10 @@ CREATE TABLE `history` (
   `tanggal` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `history`
---
-
-INSERT INTO `history` (`HistoryID`, `UserID`, `aksi`, `tanggal`) VALUES
-(118, 56, 'menambahkan postingan baru dengan caption \"mantap guys\"', '2025-05-25 11:27:43'),
-(119, 56, 'menyukai postingan dari Aliezzar Wijaya pada \"mantap guys\"', '2025-05-25 11:28:28'),
-(120, 56, 'menambahkan komentar baru dengan isi \"Mantap bang\"', '2025-05-25 11:32:13'),
-(121, 56, 'melaporkan postingan dengan alasan \"konten_tidak_pantas\"', '2025-05-25 12:12:39'),
-(122, 56, 'melaporkan postingan dengan alasan \"konten tidak pantas\"', '2025-05-25 12:12:45'),
-(123, 56, 'melaporkan postingan dengan alasan \"\"', '2025-05-25 12:19:50'),
-(124, 56, 'melaporkan postingan dengan alasan \"asdasdasdasdasdasd\"', '2025-05-25 12:26:49'),
-(125, 56, 'melaporkan postingan dengan alasan \"AOWDAKWDPAKWDPOAKDOPKAWOPKDPO\"', '2025-05-25 12:27:17'),
-(126, 56, 'Postingan anda dengan caption \"mantap guys\" telah dihapus oleh admin', '2025-05-25 12:27:28'),
-(127, 56, 'menambahkan postingan baru dengan caption \"mantap guys\"', '2025-05-25 12:28:02'),
-(128, 56, 'melaporkan postingan dengan alasan \"\"', '2025-05-25 12:28:07'),
-(129, 56, 'melaporkan postingan dengan alasan \"\"', '2025-05-25 12:30:59'),
-(130, 56, 'melaporkan postingan dengan alasan \"\"', '2025-05-25 12:40:14'),
-(131, 56, 'melaporkan postingan dengan alasan \"\"', '2025-05-25 12:40:31'),
-(132, 56, 'melaporkan postingan dengan alasan \"Konten tidak pantas\"', '2025-05-25 12:42:29'),
-(133, 56, 'Menghapus postingan \"mantap guys\"', '2025-05-25 13:10:04'),
-(134, 56, 'menambahkan postingan baru dengan caption \"aku suka kamuasd\"', '2025-05-25 13:10:39'),
-(135, 56, 'melaporkan postingan dengan alasan \"Pelanggaran hak cipta\"', '2025-05-25 13:11:04'),
-(136, 56, 'Postingan anda dengan caption \"aku suka kamuasd\" telah dihapus oleh admin', '2025-05-25 13:11:09'),
-(137, 56, 'menambahkan postingan baru dengan caption \"Horeee lihat nih gw anjing\"', '2025-05-25 15:46:00'),
-(138, 56, 'melaporkan postingan dengan alasan \"Pelanggaran hak cipta\"', '2025-05-25 15:46:13'),
-(139, 56, 'menambahkan postingan baru dengan caption \"Selamat hari sandi dunia\"', '2025-05-25 16:10:09'),
-(140, 56, 'melaporkan postingan dengan alasan \"Lainnya\"', '2025-05-25 16:10:18'),
-(141, 56, 'Postingan anda dengan caption \"Selamat hari sandi dunia\" telah dihapus oleh admin', '2025-05-25 16:10:38'),
-(142, 56, 'menambahkan postingan baru dengan caption \"aku suka kamu\"', '2025-05-25 16:11:26'),
-(143, 56, 'melaporkan postingan dengan alasan \"Pelanggaran hak cipta\"', '2025-05-25 16:11:32'),
-(144, 56, 'Postingan anda dengan caption \"aku suka kamu\" telah dihapus oleh admin', '2025-05-25 16:14:36'),
-(145, 56, 'menambahkan postingan baru dengan caption \"asd\"', '2025-05-25 16:14:56'),
-(146, 56, 'melaporkan postingan dengan alasan \"Konten tidak pantas\"', '2025-05-25 16:15:00'),
-(147, 56, 'Postingan anda dengan caption \"asd\" telah dihapus oleh admin', '2025-05-25 16:15:11'),
-(148, 56, 'menambahkan postingan baru dengan caption \"Selamat hari sandi dunia\"', '2025-05-25 16:16:44'),
-(149, 56, 'melaporkan postingan dengan alasan \"Pelanggaran hak cipta\"', '2025-05-25 16:16:55'),
-(150, 56, 'Postingan anda dengan caption \"Selamat hari sandi dunia\" telah dihapus oleh admin', '2025-05-25 16:17:03'),
-(151, 56, 'menambahkan postingan baru dengan caption \"aku suka kamu\"', '2025-05-25 16:17:59'),
-(152, 56, 'melaporkan postingan dengan alasan \"Penipuan\"', '2025-05-25 16:18:04'),
-(153, 56, 'Postingan anda dengan caption \"aku suka kamu\" telah dihapus oleh admin', '2025-05-25 16:43:21'),
-(154, 56, 'menambahkan postingan baru dengan caption \"Selamat hari sandi dunia\"', '2025-05-25 16:44:18'),
-(155, 56, 'Menghapus postingan \"Selamat hari sandi dunia\"', '2025-05-25 16:44:30'),
-(156, 56, 'menambahkan postingan baru dengan caption \"asdasasdasdas\"', '2025-05-25 16:46:03'),
-(157, 56, 'melaporkan postingan dengan alasan \"Pelanggaran hak cipta\"', '2025-05-25 16:46:27'),
-(158, 56, 'Postingan anda dengan caption \"asdasasdasdas\" telah dihapus oleh admin', '2025-05-25 16:46:56'),
-(159, 56, 'menambahkan postingan baru dengan caption \"mantap\"', '2025-05-25 16:55:12'),
-(160, 56, 'Menghapus postingan \"mantap\"', '2025-05-25 16:55:15'),
-(161, 56, 'menambahkan postingan baru dengan caption \"asdpaskoapdks\"', '2025-05-25 16:56:30'),
-(162, 56, 'menambahkan postingan baru dengan caption \"asd\"', '2025-05-25 16:57:20'),
-(163, 56, 'melaporkan postingan dengan alasan \"Pelanggaran hak cipta\"', '2025-05-25 16:57:26'),
-(164, 56, 'Postingan anda dengan caption \"asd\" telah dihapus oleh admin', '2025-05-25 16:57:36'),
-(165, 56, 'menambahkan postingan baru dengan caption \"asdasd\"', '2025-05-25 16:58:04'),
-(166, 56, 'melaporkan postingan dengan alasan \"Kekerasan\"', '2025-05-25 16:58:15'),
-(167, 56, 'menambahkan postingan baru dengan caption \"asdasdasd\"', '2025-05-25 17:02:04'),
-(168, 56, 'melaporkan postingan dengan alasan \"Penipuan\"', '2025-05-25 17:02:34'),
-(169, 56, 'menambahkan postingan baru dengan caption \"q09wekqpwe\"', '2025-05-25 17:05:37'),
-(170, 56, 'melaporkan postingan dengan alasan \"Spam\"', '2025-05-25 17:05:49'),
-(171, 56, 'Postingan anda dengan caption \"q09wekqpwe\" telah dihapus oleh admin', '2025-05-25 17:06:12'),
-(172, 56, 'Menghapus postingan \"q09wekqpwe\"', '2025-05-25 17:06:29'),
-(173, 56, 'menambahkan postingan baru dengan caption \"Selamat hari sandi dunia\"', '2025-05-25 17:13:24'),
-(174, 56, 'melaporkan postingan dengan alasan \"Konten tidak pantas\"', '2025-05-25 17:13:39'),
-(175, 56, 'melaporkan postingan dengan alasan \"Kekerasan\"', '2025-05-25 17:17:21'),
-(176, 56, 'Menghapus postingan \"Selamat hari sandi dunia\"', '2025-05-25 17:22:35'),
-(177, 56, 'menambahkan postingan baru dengan caption \"asdasdasdasd\"', '2025-05-25 17:28:35'),
-(178, 56, 'menambahkan postingan baru dengan caption \"asda\"', '2025-05-25 17:29:05'),
-(179, 56, 'melaporkan postingan dengan alasan \"Pelanggaran hak cipta\"', '2025-05-25 17:29:15'),
-(180, 56, 'Postingan anda dengan caption \"asda\" telah dihapus oleh admin', '2025-05-25 17:40:50');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `komentar`
+-- Struktur dari tabel `komentar`
 --
 
 CREATE TABLE `komentar` (
@@ -120,7 +51,7 @@ CREATE TABLE `komentar` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `laporan_pelanggaran_postingan`
+-- Struktur dari tabel `laporan_pelanggaran_postingan`
 --
 
 CREATE TABLE `laporan_pelanggaran_postingan` (
@@ -135,7 +66,7 @@ CREATE TABLE `laporan_pelanggaran_postingan` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `post`
+-- Struktur dari tabel `post`
 --
 
 CREATE TABLE `post` (
@@ -150,7 +81,7 @@ CREATE TABLE `post` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `suka`
+-- Struktur dari tabel `suka`
 --
 
 CREATE TABLE `suka` (
@@ -163,7 +94,7 @@ CREATE TABLE `suka` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -178,27 +109,25 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`UserID`, `username`, `password`, `email`, `profile_picture`, `jenis_kelamin`, `role`, `created_at`) VALUES
-(1, 'admin', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'admin@123', '6832d9c49a503.png', 'Laki-laki', 1, '2025-02-21 13:48:22'),
-(55, 'orang', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '123@123', 'default.png', NULL, 0, '2025-05-19 21:43:37'),
-(56, 'Aliezzar Wijaya', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'aliezzar42@gmail.com', 'default.png', 'Laki-laki', 0, '2025-05-19 21:46:37');
+(1, 'admin', '$2y$10$Lkl7y7TDSQ9t0HpTXXpPnOxqMpgL2NZj5EeXU0joElmHB1nnp/nG2', NULL, 'default.png', 'Laki-laki', 1, '2025-06-01 02:02:33');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `history`
+-- Indeks untuk tabel `history`
 --
 ALTER TABLE `history`
   ADD PRIMARY KEY (`HistoryID`),
   ADD KEY `UserID` (`UserID`) USING BTREE;
 
 --
--- Indexes for table `komentar`
+-- Indeks untuk tabel `komentar`
 --
 ALTER TABLE `komentar`
   ADD PRIMARY KEY (`KomentarID`),
@@ -206,7 +135,7 @@ ALTER TABLE `komentar`
   ADD KEY `FK_UserID-komentar` (`UserID`);
 
 --
--- Indexes for table `laporan_pelanggaran_postingan`
+-- Indeks untuk tabel `laporan_pelanggaran_postingan`
 --
 ALTER TABLE `laporan_pelanggaran_postingan`
   ADD PRIMARY KEY (`LaporanID`),
@@ -215,14 +144,14 @@ ALTER TABLE `laporan_pelanggaran_postingan`
   ADD KEY `FK_UserID_uploader_laporan` (`UserID_uploader`);
 
 --
--- Indexes for table `post`
+-- Indeks untuk tabel `post`
 --
 ALTER TABLE `post`
   ADD PRIMARY KEY (`PostID`),
   ADD KEY `UserID` (`UserID`);
 
 --
--- Indexes for table `suka`
+-- Indeks untuk tabel `suka`
 --
 ALTER TABLE `suka`
   ADD PRIMARY KEY (`id`),
@@ -230,71 +159,71 @@ ALTER TABLE `suka`
   ADD KEY `FK_PostID` (`PostID`) USING BTREE;
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`UserID`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `history`
+-- AUTO_INCREMENT untuk tabel `history`
 --
 ALTER TABLE `history`
-  MODIFY `HistoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=181;
+  MODIFY `HistoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=207;
 
 --
--- AUTO_INCREMENT for table `komentar`
+-- AUTO_INCREMENT untuk tabel `komentar`
 --
 ALTER TABLE `komentar`
-  MODIFY `KomentarID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=222;
+  MODIFY `KomentarID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=230;
 
 --
--- AUTO_INCREMENT for table `laporan_pelanggaran_postingan`
+-- AUTO_INCREMENT untuk tabel `laporan_pelanggaran_postingan`
 --
 ALTER TABLE `laporan_pelanggaran_postingan`
   MODIFY `LaporanID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
--- AUTO_INCREMENT for table `post`
+-- AUTO_INCREMENT untuk tabel `post`
 --
 ALTER TABLE `post`
-  MODIFY `PostID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+  MODIFY `PostID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
 
 --
--- AUTO_INCREMENT for table `suka`
+-- AUTO_INCREMENT untuk tabel `suka`
 --
 ALTER TABLE `suka`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=161;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=175;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `history`
+-- Ketidakleluasaan untuk tabel `history`
 --
 ALTER TABLE `history`
   ADD CONSTRAINT `fk_history_UserID` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `komentar`
+-- Ketidakleluasaan untuk tabel `komentar`
 --
 ALTER TABLE `komentar`
   ADD CONSTRAINT `FK_PostID_komentar` FOREIGN KEY (`PostID`) REFERENCES `post` (`PostID`) ON DELETE CASCADE,
   ADD CONSTRAINT `FK_UserID-komentar` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE;
 
 --
--- Constraints for table `laporan_pelanggaran_postingan`
+-- Ketidakleluasaan untuk tabel `laporan_pelanggaran_postingan`
 --
 ALTER TABLE `laporan_pelanggaran_postingan`
   ADD CONSTRAINT `FK_PostID_laporan` FOREIGN KEY (`PostID`) REFERENCES `post` (`PostID`) ON DELETE CASCADE,
@@ -302,13 +231,13 @@ ALTER TABLE `laporan_pelanggaran_postingan`
   ADD CONSTRAINT `FK_UserID_uploader_laporan` FOREIGN KEY (`UserID_uploader`) REFERENCES `users` (`UserID`);
 
 --
--- Constraints for table `post`
+-- Ketidakleluasaan untuk tabel `post`
 --
 ALTER TABLE `post`
   ADD CONSTRAINT `fk_user_product` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `suka`
+-- Ketidakleluasaan untuk tabel `suka`
 --
 ALTER TABLE `suka`
   ADD CONSTRAINT `FK_PostID` FOREIGN KEY (`PostID`) REFERENCES `post` (`PostID`) ON DELETE CASCADE,
